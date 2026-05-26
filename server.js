2bd178e6397642ca93805ab619b062f5c110f50f
thanseehdev <thanseehdev@gmail.com>
2026-05-27 00:40:54 +0530

    chnages

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
+app.get('/', (req, res) => {
+  res.send('D3D DROPS Server is running 🚀');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
